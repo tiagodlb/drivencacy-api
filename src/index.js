3323a5776fb40efeb9480f2782d1427adc9fcd95
Tiago <tigolimab223@gmail.com>
2022-05-18 11:11:09 -0300

    Controllers, routes done for /poll routes

--- src/index.js
+++ src/index.js
@@ -4,44 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv"
 import dayjs from "dayjs";
 import db from "./db.js";
+import pollRouter from "../routes/pollRouter.js";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(json());
 
-app.post('/poll', async (req,res) => {
-    const { title, expireAt } = req.body;
-    let now = dayjs();
-    console.log(now.format("YYYY-MM-DD HH:mm:ss"));
-
-    const poolSchema = joi.object({
-        title: joi.string().required(),
-        expireAt: joi.string().allow('').default(now.format("YYYY-MM-DD HH:mm:ss"))
-    })
-
-    const {error} = poolSchema.validate(req.body, {abortEarly: false})
-    if(error) {
-        res.status(422).send(error.details.map(detail => detail.message));
-        return;
-    }
-
-    try {
-        await db.collection("polls").insertOne({
-            title,
-            expireAt
-        })
-        console.log(expireAt)
-        res.sendStatus(201)
-    } catch (error) {
-        console.log(error)
-        res.sendStatus(422)
-    }
-
-
-})
-
-
+//routes
+app.use(pollRouter)
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
