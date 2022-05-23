1245aa06270ade15222f1e162a3e9a00d61101f8
Tiago <tigolimab223@gmail.com>
2022-05-23 15:15:15 -0300

    bug fix

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -20,7 +20,7 @@ export async function getResult(req, res) {
         votes: votes.length
       },
     });
-
+    console.log(results)
     res.send(results);
   } catch (error) {
     console.log(error);
