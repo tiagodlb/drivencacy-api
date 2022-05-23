b284492f83e843df7e93cd08732b3347a089f66a
Tiago <tigolimab223@gmail.com>
2022-05-23 13:43:21 -0300

    bug fix

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -35,6 +35,19 @@ export async function postChoiceIdVote(req, res) {
     console.log(choice)
 
     //checar aqui se já expirou
+    const pollExists = await db
+      .collection("polls")
+      .findOne({ _id: new ObjectId(poolId) });
+    console.log(pollExists.expireAt);
+    
+    const data = pollExists.expireAt;
+
+    const pollExpires = now.add();
+
+    if (pollExpires.diff(data, "day") >= 30) {
+      return res.sendStatus(403);
+    }
+
     let voteExists = await db.collection("choices").findOne({ pollId: id });
     console.log(parseInt(voteExists) + 1);
     if (!voteExists.votes) {
@@ -46,19 +59,12 @@ export async function postChoiceIdVote(req, res) {
       let voteData = await db.collection("votes").insertOne({
         title: vote,
         pollId: id,
+        choiceId: voteArray._id,
         createdAt: now.format("YYYY-MM-DD HH:mm:ss")
       });
       console.log(voteData + "votos = 1?" + " " + voteData.value);
       return res.sendStatus(201);
     }
-    console.log(voteData);
-    await db
-      .collection("choices")
-      .insertOne({
-        ...choice,
-        votes: vote + 1,
-        createdAt: now.format("YYYY-MM-DD HH:mm:ss"),
-      });
     res.sendStatus(201);
   } catch (error) {
     console.log(error);
